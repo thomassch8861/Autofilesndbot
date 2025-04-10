16ea445bdcbbf1834965665bb8d07c9a82910212
pachax001 <pachax001@gmail.com>
2025-04-10 22:13:48 +0530

    .

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1170,7 +1170,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "owner_info":
             btn = [[
                     InlineKeyboardButton("👩‍🦯 𝖡𝖺𝖼𝗄", callback_data="start"),
-                    InlineKeyboardButton("📞 𝖢𝗈𝗇𝗍𝖺𝖼𝗍", url="t.me/kichuxd")
+                    InlineKeyboardButton("📞 𝖢𝗈𝗇𝗍𝖺𝖼𝗍", url="https://t.me/matthewmurdock001")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
