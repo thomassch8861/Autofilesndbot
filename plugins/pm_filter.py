8ade794bccf3390fa69f59257e37008855278af8
pachax001 <pachax001@gmail.com>
2025-04-11 19:08:43 +0530

    Minor fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1573,9 +1573,9 @@ async def advantage_spell_chok(client, msg):
             await asyncio.sleep(600)
             await spell_check_del.delete()
     except KeyError:
-            grpid = await active_connection(str(message.from_user.id))
+            grpid = await active_connection(str(msg.from_user.id))
             await save_group_settings(grpid, 'auto_delete', True)
-            settings = await get_settings(message.chat.id)
+            settings = await get_settings(msg.chat.id)
             if settings['auto_delete']:
                 await asyncio.sleep(600)
                 await spell_check_del.delete()
